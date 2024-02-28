d900bf2f5e0d2eabbd73204562f00d0d6ae26eea
Juan Godinez <juangodinezvera@gmail.com>
2024-02-27 21:56:46 -0600

    feat: add navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 }
 
 dependencies {
+    val navigationVersion = "2.7.7"
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -44,4 +46,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navigationVersion")
 }
